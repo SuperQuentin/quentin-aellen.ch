1839e87e25381c65438784f747ab08fc1e97d44a
SuperQuentin <quentin.aellen@infrack.ch>
2022-10-22 23:08:20 +0200

    feat: 🎸 add display of blog card

--- components/Blog/Card.tsx
+++ components/Blog/Card.tsx
@@ -14,22 +14,29 @@ export enum GradientOrientation {
 interface CardProps {
     children: ReactElement;
     href: string;
+    title: string;
     imageSrc?: string;
     gradientOrientation?: GradientOrientation;
 }
 
 export const Card = (props: CardProps) => {
 
-    const { children, href, imageSrc, gradientOrientation } = props;
+    const { children, href, title, imageSrc, gradientOrientation } = props;
 
     return (
-        <a className="flex flex-col flex-1 col-span-3 p-8 transition bg-white shadow-2xl rounded-3xl hover:cursor-pointer" href={href}>
-            <div className="relative h-full">
-                <Image className="absolute" src={imageSrc ?? "https://source.unsplash.com/_KsnSQoKhUQ"} layout="fill" objectFit="cover" />
+        <a className="flex flex-col flex-1 col-span-4 transition bg-white shadow-2xl rounded-3xl hover:cursor-pointer" href={href} title={title}>
+            <div className="relative h-40">
+                <div className="absolute inset-0 z-10 bg-gradient-to-t from-black via-transparent to-transparent"></div>
+                <Image className="absolute rounded-t-3xl" src={imageSrc ?? "https://source.unsplash.com/_KsnSQoKhUQ"} layout="fill" objectFit="cover" />
             </div>
 
-            {children}
-
+            <div className="relative z-0 px-5 pt-3 pb-6 space-y-2">
+                <div className="absolute inset-0 z-10 rounded-b-3xl bg-gradient-to-t from-white to-transparent"></div>
+                <h2 className="text-2xl font-bold truncate">{title}</h2>
+                <div className="h-16 overflow-hidden md:h-36 ">
+                    {children}
+                </div>
+            </div>
         </a>
     )
 }
--- components/Layouts/BlogLayout.tsx
+++ components/Layouts/BlogLayout.tsx
@@ -1,5 +1,6 @@
 import React, { ReactElement } from "react"
 import Head from 'next/head'
+import Image from "next/image"
 
 export const BlogLayout = ({ children }: { children: ReactElement }) => {
     return (
@@ -9,8 +10,16 @@ export const BlogLayout = ({ children }: { children: ReactElement }) => {
                 <meta name="description" content="Quentin Aellen portfolio blog" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <main className="min-h-screen bg-blue-300">
-                <div className="grid grid-cols-12 grid-rows-2 mx-auto max-w-7xl">
+            <main className="min-h-screen space-y-4 bg-blue-300">
+                <div className="relative z-0 h-48 md:mb-12 md:h-96">
+                    <div className="absolute inset-0 -z-10 bg-gradient-to-t from-black via-transparent to-transparent"></div>
+                    <Image className="absolute rounded-t-3xl -z-20" src={"https://source.unsplash.com/_KsnSQoKhUQ"} layout="fill" objectFit="cover" />
+
+                    <div className="flex flex-col-reverse h-full px-4 py-6 mx-auto max-w-7xl">
+                        <h1 className="text-6xl font-bold text-white">Blog</h1>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-5 px-4 mx-auto md:grid-rows-2 md:grid md:grid-cols-12 max-w-7xl">
                     {children}
                 </div>
             </main>
