dd6be819fef23a7853c995940879591cdf6afa35
SuperQuentin <quentin.aellen@infrack.ch>
2022-10-24 20:51:45 +0200

    feat: 🎸 improve hover on blog card

--- components/Blog/Card.tsx
+++ components/Blog/Card.tsx
@@ -24,7 +24,7 @@ export const Card = (props: CardProps) => {
     const { children, href, title, imageSrc, gradientOrientation } = props;
 
     return (
-        <a className="flex flex-col flex-1 col-span-4 transition bg-white shadow-2xl rounded-3xl hover:cursor-pointer" href={href} title={title}>
+        <a className="flex flex-col flex-1 col-span-4 transition bg-white shadow-2xl group rounded-3xl hover:cursor-pointer hover:scale-95 active:scale-90" href={href} title={title}>
             <div className="relative h-40">
                 <div className="absolute inset-0 z-10 bg-gradient-to-t from-black via-transparent to-transparent"></div>
                 <Image className="absolute rounded-t-3xl" src={imageSrc ?? "https://source.unsplash.com/_KsnSQoKhUQ"} layout="fill" objectFit="cover" />
@@ -32,7 +32,7 @@ export const Card = (props: CardProps) => {
 
             <div className="relative z-0 px-5 pt-3 pb-6 space-y-2">
                 <div className="absolute inset-0 z-10 rounded-b-3xl bg-gradient-to-t from-white to-transparent"></div>
-                <h2 className="text-2xl font-bold truncate">{title}</h2>
+                <h2 className="text-2xl font-bold truncate group-hover:text-orange-600">{title}</h2>
                 <div className="h-16 overflow-hidden md:h-36 ">
                     {children}
                 </div>
