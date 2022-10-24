f79c6bf4d379109cac2f03381530da5fafa4d859
SuperQuentin <quentin.aellen@infrack.ch>
2022-10-24 21:33:44 +0200

    fix: 🐛 add <Link> to navigate in nextjs instead of only <a>

--- components/Welcome/Cards.tsx
+++ components/Welcome/Cards.tsx
@@ -3,6 +3,7 @@ import { Children, ReactElement } from "react";
 import Image from 'next/image';
 
 import clsx from 'clsx';
+import Link from "next/link";
 
 export enum GradientOrientation {
     Up = "bg-gradient-to-t",
@@ -14,23 +15,26 @@ export enum GradientOrientation {
 interface CardProps {
     children: ReactElement;
     href: string;
+    title: string;
     imageSrc?: string;
     gradientOrientation?: GradientOrientation;
 }
 
 export const Card = (props: CardProps) => {
 
-    const { children, href, imageSrc, gradientOrientation } = props;
+    const { children, href, title, imageSrc, gradientOrientation } = props;
 
     return (
-        <a className="relative z-0 flex-1 p-8 transition shadow-2xl group rounded-3xl hover:scale-95 active:scale-90 hover:-rotate-2 hover:cursor-pointer" href={href}>
-            <div className="absolute inset-0 -z-20 rounded-3xl">
-                <div className={clsx("absolute inset-0 transition -z-20 from-transparent via-transparent to-black group-hover:to-orange-500 opacity-60 rounded-3xl", gradientOrientation?.toString() ?? GradientOrientation.Down.toString())}></div>
-                <Image className="rounded-3xl -z-30" src={imageSrc ?? "https://source.unsplash.com/_KsnSQoKhUQ"} layout="fill" objectFit="cover" />
-            </div>
+        <Link href={href} title={title}>
+            <a className="relative z-0 flex-1 p-8 transition shadow-2xl group rounded-3xl hover:scale-95 active:scale-90 hover:-rotate-2 hover:cursor-pointer">
+                <div className="absolute inset-0 -z-20 rounded-3xl">
+                    <div className={clsx("absolute inset-0 transition -z-20 from-transparent via-transparent to-black group-hover:to-orange-500 opacity-60 rounded-3xl", gradientOrientation?.toString() ?? GradientOrientation.Down.toString())}></div>
+                    <Image className="rounded-3xl -z-30" src={imageSrc ?? "https://source.unsplash.com/_KsnSQoKhUQ"} layout="fill" objectFit="cover" />
+                </div>
 
-            {children}
+                {children}
 
-        </a>
+            </a>
+        </Link>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,26 +8,26 @@ const Home: NextPageWithLayout = () => {
     <section className="flex flex-col items-center justify-center min-h-screen p-5">
       <div className="flex flex-col justify-center flex-1 w-full px-4 max-w-7xl">
         <div className="flex flex-1 gap-4 md:flex-row max-h-[720px]">
-          <Card href="#" imageSrc="/images/moto.jpg">
+          <Card href="#" imageSrc="/images/moto.jpg" title="About me">
             <div className="flex flex-col-reverse items-end h-full">
               <h2 className="font-sans text-3xl text-white fond-bold">À propos de moi</h2>
             </div>
           </Card>
 
           <div className="flex flex-col flex-1 gap-4">
-            <Card href="#" gradientOrientation={GradientOrientation.Up}>
+            <Card href="#" gradientOrientation={GradientOrientation.Up} title="Projects">
               <div className="flex flex-col h-full">
                 <h2 className="font-sans text-3xl text-white fond-bold">Projets</h2>
               </div>
             </Card>
 
             <div className="flex flex-col flex-1 gap-4 md:flex-row">
-              <Card href="/blog" gradientOrientation={GradientOrientation.Up}>
+              <Card href="/blog" gradientOrientation={GradientOrientation.Up} title="Blog">
                 <div className="flex flex-col h-full">
                   <h2 className="font-sans text-3xl text-white fond-bold">Blog</h2>
                 </div>
               </Card>
-              <Card href="#" gradientOrientation={GradientOrientation.Up}>
+              <Card href="#" gradientOrientation={GradientOrientation.Up} title="Contact">
                 <div className="flex flex-col h-full">
                   <h2 className="font-sans text-3xl text-white fond-bold">Contact</h2>
                 </div>
