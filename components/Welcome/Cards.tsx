059bb61f8d48c680a3fde1cd81267be1d0767638
SuperQuentin <quentin.aellen@infrack.ch>
2023-01-25 16:39:42 +0100

    feat: improve display on welcome page

--- components/Welcome/Cards.tsx
+++ components/Welcome/Cards.tsx
@@ -1,40 +1,50 @@
 import { Children, ReactElement } from "react";
 
-import Image from 'next/image';
+import Image from "next/image";
 
-import clsx from 'clsx';
+import clsx from "clsx";
 import Link from "next/link";
 
 export enum GradientOrientation {
-    Up = "bg-gradient-to-t",
-    Down = "bg-gradient-to-b",
-    Left = "bg-gradient-to-l",
-    Right = "bg-gradient-to-r"
+  Up = "bg-gradient-to-t",
+  Down = "bg-gradient-to-b",
+  Left = "bg-gradient-to-l",
+  Right = "bg-gradient-to-r",
 }
 
 interface CardProps {
-    children: ReactElement;
-    href: string;
-    title: string;
-    imageSrc?: string;
-    gradientOrientation?: GradientOrientation;
+  children: ReactElement;
+  href: string;
+  title: string;
+  imageSrc?: string;
+  gradientOrientation?: GradientOrientation;
 }
 
 export const Card = (props: CardProps) => {
+  const { children, href, title, imageSrc, gradientOrientation } = props;
 
-    const { children, href, title, imageSrc, gradientOrientation } = props;
+  return (
+    <Link href={href} title={title}>
+      <a className="relative z-0 flex-1 p-8 transition shadow-2xl boxbox group rounded-3xl hover:scale-95 active:scale-90 hover:-rotate-2 hover:cursor-pointer">
+        <div className="absolute inset-0 -z-20 rounded-3xl">
+          <div
+            className={clsx(
+              "absolute inset-0 transition -z-20 from-transparent via-transparent to-black group-hover:to-orange-500 opacity-60 rounded-3xl",
+              gradientOrientation?.toString() ??
+                GradientOrientation.Down.toString()
+            )}
+          ></div>
+          <Image
+            className="rounded-3xl -z-30"
+            src={imageSrc ?? "https://source.unsplash.com/_KsnSQoKhUQ"}
+            layout="fill"
+            objectFit="cover"
+            alt={title + " background button image"}
+          />
+        </div>
 
-    return (
-        <Link href={href} title={title}>
-            <a className="relative z-0 flex-1 p-8 transition shadow-2xl group rounded-3xl hover:scale-95 active:scale-90 hover:-rotate-2 hover:cursor-pointer">
-                <div className="absolute inset-0 -z-20 rounded-3xl">
-                    <div className={clsx("absolute inset-0 transition -z-20 from-transparent via-transparent to-black group-hover:to-orange-500 opacity-60 rounded-3xl", gradientOrientation?.toString() ?? GradientOrientation.Down.toString())}></div>
-                    <Image className="rounded-3xl -z-30" src={imageSrc ?? "https://source.unsplash.com/_KsnSQoKhUQ"} layout="fill" objectFit="cover" alt={title + " background button image"} />
-                </div>
-
-                {children}
-
-            </a>
-        </Link>
-    )
-}
+        {children}
+      </a>
+    </Link>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,50 +1,67 @@
-import { ReactElement } from 'react'
-import { WelcomeLayout } from '../components/Layouts/WelcomeLayout'
-import { NextPageWithLayout } from './_app'
-import { Card, GradientOrientation } from '../components/Welcome/Cards'
+import { ReactElement } from "react";
+import { WelcomeLayout } from "../components/Layouts/WelcomeLayout";
+import { NextPageWithLayout } from "./_app";
+import { Card, GradientOrientation } from "../components/Welcome/Cards";
 
 const Home: NextPageWithLayout = () => {
   return (
-    <section className="flex flex-col items-center justify-center min-h-screen p-5">
+    <section className="flex flex-col items-center justify-center min-h-screen px-6 py-6 md:py-24">
       <div className="flex flex-col justify-center flex-1 w-full md:px-4 max-w-7xl">
         <div className="flex flex-col flex-1 gap-4 md:flex-row max-h-[720px]">
           <Card href="/me" imageSrc="/images/moto.jpg" title="About me">
             <div className="flex flex-col-reverse items-end h-full">
-              <h2 className="font-sans text-3xl text-white fond-bold">À propos de moi</h2>
+              <h2 className="font-sans text-3xl text-white fond-bold">
+                À propos de moi
+              </h2>
             </div>
           </Card>
 
           <div className="flex flex-col flex-1 gap-4">
-            <Card href="#" gradientOrientation={GradientOrientation.Up} title="Projects">
+            <Card
+              href="#"
+              gradientOrientation={GradientOrientation.Up}
+              title="Projects"
+            >
               <div className="flex flex-col h-full">
-                <h2 className="font-sans text-3xl text-white fond-bold">Projets</h2>
+                <h2 className="font-sans text-3xl text-white fond-bold">
+                  Projets
+                </h2>
               </div>
             </Card>
 
             <div className="flex flex-col flex-1 gap-4 md:flex-row">
-              <Card href="/blog" gradientOrientation={GradientOrientation.Up} title="Blog">
+              <Card
+                href="/blog"
+                gradientOrientation={GradientOrientation.Up}
+                title="Blog"
+              >
                 <div className="flex flex-col h-full">
-                  <h2 className="font-sans text-3xl text-white fond-bold">Blog</h2>
+                  <h2 className="font-sans text-3xl text-white fond-bold">
+                    Blog
+                  </h2>
                 </div>
               </Card>
-              <Card href="#" gradientOrientation={GradientOrientation.Up} title="Contact">
+              <Card
+                href="#"
+                gradientOrientation={GradientOrientation.Up}
+                title="Contact"
+              >
                 <div className="flex flex-col h-full">
-                  <h2 className="font-sans text-3xl text-white fond-bold">Contact</h2>
+                  <h2 className="font-sans text-3xl text-white fond-bold">
+                    Contact
+                  </h2>
                 </div>
               </Card>
             </div>
           </div>
-
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
 Home.getLayout = function getLayout(page: ReactElement) {
-  return (
-    <WelcomeLayout>{page}</WelcomeLayout>
-  )
-}
+  return <WelcomeLayout>{page}</WelcomeLayout>;
+};
 
-export default Home
+export default Home;
